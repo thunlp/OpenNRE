3632dddd77144b5890d6c6f78b2ea6e83d9572b7
SillyXu <thu.hanxu13@gmail.com>
2019-10-28 15:23:34 +0800

    Update train_wiki80_bert_softmax.py

--- example/train_wiki80_bert_softmax.py
+++ example/train_wiki80_bert_softmax.py
@@ -1,31 +1,32 @@
-# coding:utf-8
-import sys
-sys.path.append('.')
-
+import sys, json
 import torch
 import numpy as np
-import json
 import opennre
 from opennre import encoder, model, framework
-
+# Some basic settings
+sys.path.append('.')
 ckpt = 'ckpt/wiki80_bert_softmax.pth.tar'
 rel2id = json.load(open('benchmark/wiki80/wiki80_rel2id.json'))
+# Define the sentence encoder.
 sentence_encoder = opennre.encoder.BERTEncoder(
     max_length=80, pretrain_path='pretrain/bert-base-uncased')
+# Define the model.
 model = opennre.model.SoftmaxNN(sentence_encoder, len(rel2id), rel2id)
+# Define the whole training framework.
 framework = opennre.framework.SentenceRE(
-    train_path='benchmark/wiki80/wiki80_train.txt',
-    val_path='benchmark/wiki80/wiki80_val.txt',
-    test_path='benchmark/wiki80/wiki80_val.txt',
-    model=model,
-    ckpt=ckpt,
-    batch_size=64,
-    max_epoch=10,
-    lr=2e-5,
-    opt='adamw')
-# Train
+    train_path = 'benchmark/wiki80/wiki80_train.txt',
+    val_path   = 'benchmark/wiki80/wiki80_val.txt',
+    test_path  = 'benchmark/wiki80/wiki80_val.txt',
+    model      = model,
+    ckpt       = ckpt,
+    batch_size = 64,
+    max_epoch  = 10,
+    lr         = 2e-5,
+    opt        = 'adamw')
+# Train the model
 framework.train_model()
-# Test
+# Test the model
 framework.load_state_dict(torch.load(ckpt)['state_dict'])
 result = framework.eval_model(framework.test_loader)
+# Print the result
 print('Accuracy on test set: {}'.format(result['acc']))
