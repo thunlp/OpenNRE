4879b59d8416af9635f35fc8f8afa1d274243c54
Tianyu Gao <gaotianyu1350@126.com>
2019-10-30 03:48:09 +0000

    add download script for bert and glove

---
+++ pretrain/download_bert.sh
@@ -0,0 +1,4 @@
+mkdir bert-base-uncased
+wget -P bert-base-uncased http://193.112.16.83:8080/opennre/pretrain/bert-base-uncased/config.json
+wget -P bert-base-uncased http://193.112.16.83:8080/opennre/pretrain/bert-base-uncased/pytorch_model.bin
+wget -P bert-base-uncased http://193.112.16.83:8080/opennre/pretrain/bert-base-uncased/vocab.txt
---
+++ pretrain/download_glove.sh
@@ -0,0 +1,3 @@
+mkdir glove
+wget -P glove http://193.112.16.83:8080/opennre/pretrain/glove/glove.6B.50d_mat.npy
+wget -P glove http://193.112.16.83:8080/opennre/pretrain/glove/glove.6B.50d_word2id.json
