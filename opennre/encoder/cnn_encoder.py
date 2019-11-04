063d65230da178aef10e08b470a87d24ddb44970
Tianyu Gao <gaotianyu1350@126.com>
2019-11-04 14:03:50 +0800

    fix some bug

--- opennre/encoder/cnn_encoder.py
+++ opennre/encoder/cnn_encoder.py
@@ -39,7 +39,7 @@ class CNNEncoder(BaseEncoder):
         self.padding_size = padding_size
         self.act = activation_function
 
-        self.conv = nn.Conv1d(self.input_size, self.hidden_size, self.kernel_size, padding=self.padding)
+        self.conv = nn.Conv1d(self.input_size, self.hidden_size, self.kernel_size, padding=self.padding_size)
         self.pool = nn.MaxPool1d(self.max_length)
 
     def forward(self, token, pos1, pos2):
