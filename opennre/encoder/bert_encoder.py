fix tokenization bug when masking entities

opennre/encoder/bert_encoder.py
@@ -48,63 +48,47 @@ class BERTEncoder(nn.Module):
         pos_head = item['h']['pos']
         pos_tail = item['t']['pos']
 
+        pos_min = pos_head
+        pos_max = pos_tail
+        if pos_head[0] > pos_tail[0]:
+            pos_min = pos_tail
+            pos_max = pos_head
+            rev = True
+        else:
+            rev = False
+        
         if not is_token:
-            pos_min = pos_head
-            pos_max = pos_tail
-            if pos_head[0] > pos_tail[0]:
-                pos_min = pos_tail
-                pos_max = pos_head
-                rev = True
-            else:
-                rev = False
             sent0 = self.tokenizer.tokenize(sentence[:pos_min[0]])
             ent0 = self.tokenizer.tokenize(sentence[pos_min[0]:pos_min[1]])
             sent1 = self.tokenizer.tokenize(sentence[pos_min[1]:pos_max[0]])
             ent1 = self.tokenizer.tokenize(sentence[pos_max[0]:pos_max[1]])
             sent2 = self.tokenizer.tokenize(sentence[pos_max[1]:])
-            if self.mask_entity:
-                ent0 = ['[unused4]']
-                ent1 = ['[unused5]']
-                if rev:
-                    ent0 = ['[unused5]']
-                    ent1 = ['[unused4]']
-            pos_head = [len(sent0), len(sent0) + len(ent0)]
-            pos_tail = [
-                len(sent0) + len(ent0) + len(sent1),
-                len(sent0) + len(ent0) + len(sent1) + len(ent1)
-            ]
-            if rev:
-                pos_tail = [len(sent0), len(sent0) + len(ent0)]
-                pos_head = [
-                    len(sent0) + len(ent0) + len(sent1),
-                    len(sent0) + len(ent0) + len(sent1) + len(ent1)
-                ]
-            tokens = sent0 + ent0 + sent1 + ent1 + sent2
         else:
-            tokens = sentence
+            sent0 = self.tokenizer.tokenize(' '.join(sentence[:pos_min[0]]))
+            ent0 = self.tokenizer.tokenize(' '.join(sentence[pos_min[0]:pos_min[1]]))
+            sent1 = self.tokenizer.tokenize(' '.join(sentence[pos_min[1]:pos_max[0]]))
+            ent1 = self.tokenizer.tokenize(' '.join(sentence[pos_max[0]:pos_max[1]]))
+            sent2 = self.tokenizer.tokenize(' '.join(sentence[pos_max[1]:]))
 
-        # Token -> index
-        re_tokens = ['[CLS]']
-        cur_pos = 0
-        for token in tokens:
-            token = token.lower()
-            if cur_pos == pos_head[0] and not self.mask_entity:
-                re_tokens.append('[unused0]')
-            if cur_pos == pos_tail[0] and not self.mask_entity:
-                re_tokens.append('[unused1]')
-            if is_token:
-                re_tokens += self.tokenizer.tokenize(token)
-            else:
-                re_tokens.append(token)
-            if cur_pos == pos_head[1] - 1 and not self.mask_entity:
-                re_tokens.append('[unused2]')
-            if cur_pos == pos_tail[1] - 1 and not self.mask_entity:
-                re_tokens.append('[unused3]')
-            cur_pos += 1
-        re_tokens.append('[SEP]')
+        if self.mask_entity:
+            ent0 = ['[unused4]'] if not rev else ['[unused5]']
+            ent1 = ['[unused5]'] if not rev else ['[unused4]']
+        else:
+            ent0 = ['[unused0]'] + ent0 + ['[unused1]'] if not rev else ['[unused2]'] + ent0 + ['[unused3]']
+            ent1 = ['[unused2]'] + ent1 + ['[unused3]'] if not rev else ['[unused0]'] + ent1 + ['[unused1]']
 
+        re_tokens = ['[CLS]'] + sent0 + ent0 + sent1 + ent1 + sent2 + ['[SEP]']
+        pos1 = 1 + len(sent0) if not rev else 1 + len(sent0 + ent0 + sent1)
+        pos2 = 1 + len(sent0 + ent0 + sent1) if not rev else 1 + len(sent0)
+        pos1 = min(self.max_length - 1, pos1)
+        pos2 = min(self.max_length - 1, pos2)
+        
         indexed_tokens = self.tokenizer.convert_tokens_to_ids(re_tokens)
         avai_len = len(indexed_tokens)
+
+        # Position
+        pos1 = torch.tensor([[pos1]]).long()
+        pos2 = torch.tensor([[pos2]]).long()
 
         # Padding
         if self.blank_padding:
@@ -176,67 +160,41 @@ class BERTEntityEncoder(nn.Module):
         pos_head = item['h']['pos']
         pos_tail = item['t']['pos']
 
+        pos_min = pos_head
+        pos_max = pos_tail
+        if pos_head[0] > pos_tail[0]:
+            pos_min = pos_tail
+            pos_max = pos_head
+            rev = True
+        else:
+            rev = False
+        
         if not is_token:
-            pos_min = pos_head
-            pos_max = pos_tail
-            if pos_head[0] > pos_tail[0]:
-                pos_min = pos_tail
-                pos_max = pos_head
-                rev = True
-            else:
-                rev = False
             sent0 = self.tokenizer.tokenize(sentence[:pos_min[0]])
             ent0 = self.tokenizer.tokenize(sentence[pos_min[0]:pos_min[1]])
             sent1 = self.tokenizer.tokenize(sentence[pos_min[1]:pos_max[0]])
             ent1 = self.tokenizer.tokenize(sentence[pos_max[0]:pos_max[1]])
             sent2 = self.tokenizer.tokenize(sentence[pos_max[1]:])
-            if self.mask_entity:
-                ent0 = ['[unused4]']
-                ent1 = ['[unused5]']
-                if rev:
-                    ent0 = ['[unused5]']
-                    ent1 = ['[unused4]']
-            pos_head = [len(sent0), len(sent0) + len(ent0)]
-            pos_tail = [
-                len(sent0) + len(ent0) + len(sent1),
-                len(sent0) + len(ent0) + len(sent1) + len(ent1)
-            ]
-            if rev:
-                pos_tail = [len(sent0), len(sent0) + len(ent0)]
-                pos_head = [
-                    len(sent0) + len(ent0) + len(sent1),
-                    len(sent0) + len(ent0) + len(sent1) + len(ent1)
-                ]
-            tokens = sent0 + ent0 + sent1 + ent1 + sent2
         else:
-            tokens = sentence
+            sent0 = self.tokenizer.tokenize(' '.join(sentence[:pos_min[0]]))
+            ent0 = self.tokenizer.tokenize(' '.join(sentence[pos_min[0]:pos_min[1]]))
+            sent1 = self.tokenizer.tokenize(' '.join(sentence[pos_min[1]:pos_max[0]]))
+            ent1 = self.tokenizer.tokenize(' '.join(sentence[pos_max[0]:pos_max[1]]))
+            sent2 = self.tokenizer.tokenize(' '.join(sentence[pos_max[1]:]))
 
-        # Token -> index
-        re_tokens = ['[CLS]']
-        cur_pos = 0
-        pos1 = 0
-        pos2 = 0
-        for token in tokens:
-            token = token.lower()
-            if cur_pos == pos_head[0]:
-                pos1 = len(re_tokens)
-                re_tokens.append('[unused0]')
-            if cur_pos == pos_tail[0]:
-                pos2 = len(re_tokens)
-                re_tokens.append('[unused1]')
-            if is_token:
-                re_tokens += self.tokenizer.tokenize(token)
-            else:
-                re_tokens.append(token)
-            if cur_pos == pos_head[1] - 1:
-                re_tokens.append('[unused2]')
-            if cur_pos == pos_tail[1] - 1:
-                re_tokens.append('[unused3]')
-            cur_pos += 1
-        re_tokens.append('[SEP]')
+        if self.mask_entity:
+            ent0 = ['[unused4]'] if not rev else ['[unused5]']
+            ent1 = ['[unused5]'] if not rev else ['[unused4]']
+        else:
+            ent0 = ['[unused0]'] + ent0 + ['[unused1]'] if not rev else ['[unused2]'] + ent0 + ['[unused3]']
+            ent1 = ['[unused2]'] + ent1 + ['[unused3]'] if not rev else ['[unused0]'] + ent1 + ['[unused1]']
+
+        re_tokens = ['[CLS]'] + sent0 + ent0 + sent1 + ent1 + sent2 + ['[SEP]']
+        pos1 = 1 + len(sent0) if not rev else 1 + len(sent0 + ent0 + sent1)
+        pos2 = 1 + len(sent0 + ent0 + sent1) if not rev else 1 + len(sent0)
         pos1 = min(self.max_length - 1, pos1)
         pos2 = min(self.max_length - 1, pos2)
-
+        
         indexed_tokens = self.tokenizer.convert_tokens_to_ids(re_tokens)
         avai_len = len(indexed_tokens)
 
@@ -249,8 +207,7 @@ class BERTEntityEncoder(nn.Module):
             while len(indexed_tokens) < self.max_length:
                 indexed_tokens.append(0)  # 0 is id for [PAD]
             indexed_tokens = indexed_tokens[:self.max_length]
-        indexed_tokens = torch.tensor(indexed_tokens).long().unsqueeze(
-            0)  # (1, L)
+        indexed_tokens = torch.tensor(indexed_tokens).long().unsqueeze(0)  # (1, L)
 
         # Attention mask
         att_mask = torch.zeros(indexed_tokens.size()).long()  # (1, L)